d0b38a8f534d7ff799be0fc8557bd706f12cc6dc
wention <xuyi.828@gmail.com>
2025-03-26 14:22:38 +0800

    update retag scripts

--- ci/gitea/setup.sh
+++ ci/gitea/setup.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 ARCHS="amd64 arm64"
-TAGS="ubuntu-latest ubuntu-22.04 ubuntu-20.04"
+TAGS="ubuntu-latest ubuntu-24.04 ubuntu-22.04 ubuntu-20.04"
 
 for arch in $ARCHS;do
   for tag in $TAGS;do
     echo pull, retag and push linux/${arch} gitea/runner-images:${tag}
     docker pull --platform linux/${arch} gitea/runner-images:${tag}
     docker image tag gitea/runner-images:${tag} wention/gitea-runner-images:${tag}-${arch}
-    docker push --platform linux/${arch} wention/gitea-runner-images:${tag}-${arch}
+    docker push wention/gitea-runner-images:${tag}-${arch}
   done
 done
 
