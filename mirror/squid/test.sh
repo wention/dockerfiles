3a8fda7f2711f5f790b52da5c6e2bd4ee5b9525b
wention <xuyi.828@gmail.com>
2025-04-01 05:39:38 +0000

    update config

--- mirror/squid/test.sh
+++ mirror/squid/test.sh
@@ -3,7 +3,8 @@
 docker run --rm -i ubuntu:22.04 bash <<EOF
 export DEBIAN_FRONTEND=noninteractive
 export TZ=Asia/Shanghai
+export http_proxy=http://192.168.32.121:3128
 
-http_proxy=http://192.168.32.121:3128 apt -o pkgProblemResolver=true -o Acquire::http=true update -y
-http_proxy=http://192.168.32.121:3128 apt -o pkgProblemResolver=true -o Acquire::http=true install -y cmake build-essential
+apt update -y
+apt install -y cmake build-essential
 EOF
